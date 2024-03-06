5718b7a97f80216db95c4134ff011204bb950308
darkphase01 <darkphase01@users.noreply.github.com>
2024-03-06 15:57:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { NavBarHeader, CardRonald, MarketingFooter } from './ui-components'
+import { NavBarHeader, CardRonaldHomeCollection, MarketingFooter } from './ui-components'
 
 
 function App() {
 return (
   <>
     <NavBarHeader width="100%" marginBottom='20px' />
-    <CardRonald marginBottom='20px' />
+    <CardRonaldHomeCollection marginBottom='20px' />
     <MarketingFooter width='100%' />
   </>
 )
